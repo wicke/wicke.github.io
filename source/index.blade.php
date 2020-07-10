bc7cbc83d21e908a98b99538e0d9b186df8cbf95
Chad Simpson <wicke0719@gmail.com>
2020-07-10 15:58:17 -0400

    updated index page

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,19 +1,19 @@
 @extends('_layouts.master')
 
 @section('body')
-<div class="mx-auto w-3/4 m-10 p-10 bg-white rounded-md border-2 border-black  text-center">
+<div class="mx-auto w-3/4 m-6 p-10 bg-white rounded-md border-2 border-black  text-center">
     <h1 class="text-3xl">CHADWICKE SPENSER SIMPSON</h1>
 </div> 
 
-<div class="mx-auto w-3/4 m-10 p-10 bg-orange-300 rounded-md border-2 border-black  text-center">
+<div class="mx-auto w-3/4 m-6 p-10 bg-orange-300 rounded-md border-2 border-black  text-center">
     <h1 class="text-3xl">TARA LYNN SIMPSON</h1>
 </div> 
 
-<div class="mx-auto w-3/4 m-10 p-10 bg-yellow-300 rounded-md border-2 border-black  text-center">
+<div class="mx-auto w-3/4 m-6 p-10 bg-yellow-300 rounded-md border-2 border-black  text-center">
     <h1 class="text-3xl">SPENSER WADE SIMPSON</h1>
 </div> 
 
-<div class="mx-auto w-3/4 m-10 p-10 bg-green-300 rounded-md border-2 border-black  text-center">
+<div class="mx-auto w-3/4 m-6 p-10 bg-green-300 rounded-md border-2 border-black  text-center">
     <h1 class="text-3xl">BRETT WILLIAM SIMPSON</h1>
 </div> 
 @endsection
