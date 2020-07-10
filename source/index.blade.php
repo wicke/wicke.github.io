bb14e493ca1a2440bd03e116346a9eef06a68bf0
Chad Simpson <wicke0719@gmail.com>
2020-07-10 14:04:36 -0400

    initial commit

---
+++ source/index.blade.php
@@ -0,0 +1,8 @@
+@extends('_layouts.master')
+
+@section('body')
+<div class="mx-auto w-3/4 m-10 p-10 bg-white rounded-md border-2 border-black  text-center">
+    <h1 class="text-3xl">CHADWICKE SPENSER SIMPSON</h1>
+</div> 
+@endsection
+ 
---
+++ webpack.mix.js
@@ -0,0 +1,20 @@
+const mix = require('laravel-mix');
+const tailwindcss = require('tailwindcss')
+require('laravel-mix-jigsaw');
+
+mix.disableSuccessNotifications();
+mix.setPublicPath('source/assets/build');
+
+mix.jigsaw()
+    .js('source/_assets/js/main.js', 'js')
+    .sass('source/_assets/sass/main.scss', 'css')    
+    .options({
+        processCssUrls: false,
+    })
+    .version();
+
+mix.sass('source/_assets/sass/app.scss', 'css')
+  .options({
+    processCssUrls: false,
+    postCss: [ tailwindcss('./tailwind.config.js') ],
+  }).version();
